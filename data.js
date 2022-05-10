2cdf1eafe8cdb75aa2c073649f702d84ef2109d2
unknown <eliasjrcosta886@gmail.com>
2022-05-10 02:47:22 -0300

    adição alteração na lista de audios

--- data.js
+++ data.js
@@ -2,20 +2,83 @@ window.audios = [
     {
       title:
         "Ana Gabriela - Céu Azul (Áudio Oficial)",
-      artist: "Desconhecido",
-      cover: "img1.gif",
+      artist: "Ana Gabriela",
+      cover: "img11.gif",
       file: "Ana Gabriela - Céu Azul (Áudio Oficial).mp3"
+    },
+    {
+      title: "_TOP 5_ - Kiesel Solo Contest 2017 - Leticia Filizzola",
+      artist: "Leticia Filizzol",
+      cover: "img16.webp",
+      file: "_TOP 5_ - Kiesel Solo Contest 2017 - Leticia Filizzola.mp3"
     },
     {
       title: "Future Funk",
       artist: "Hope",
-      cover: "img2.gif",
+      cover: "img12.gif",
       file: "Future Funk.mp3"
     },
     {
       title: "Happy Sad _ Ruben Wan (Original).mp3",
       artist: "Desconhecido",
-      cover: "img3.jpg",
+      cover: "img13.webp",
       file: "Happy Sad _ Ruben Wan (Original).mp3"
-    }
+    },
+    {
+      title: "_1ST PLACE_ Kiesel Guitars Solo Contest 2017 - Aryz Bulo",
+      artist: "Aryz Bulo",
+      cover: "img18.gif",
+      file: "_1ST PLACE_ Kiesel Guitars Solo Contest 2017 - Aryz Bulo.mp3"
+    },
+    
+    {
+      title: "Rose __ Ruben Wan (Original)",
+      artist: "Ruben Wan",
+      cover: "img17.gif",
+      file: "Rose __ Ruben Wan (Original).mp3"
+    },
+    {
+      title: "_2ND PLACE - Kiesel Solo Contest2017 - Brunno Henrique",
+      artist: "Brunno Henrique",
+      cover: "img15.gif",
+      file: "_2ND PLACE - Kiesel Solo Contest2017 - Brunno Henrique.mp3"
+    },
+    {
+      title: "Ruben Wan _ God Complex (Official Music Video).mp3",
+      artist: "Ruben Wan",
+      cover: "img14.gif",
+      file: "Ruben Wan _ God Complex (Official Music Video).mp3"
+    },
+    {
+      title: "Dear God....mp3",
+      artist: "Mateus Asato",
+      cover: "img20.gif",
+      file: "Dear God....mp3"
+    },
+    {
+      title: "Mateus Asato + Neural - a beautiful combo ❤️‍🔥🤝⚡️.mp3",
+      artist: "Mateus Asato",
+      cover: "img21.gif",
+      file: "Mateus Asato + Neural - a beautiful combo ❤️‍🔥🤝⚡️.mp3"
+    },
+
+    {
+      title: "NOVELISTS FR - Echoes",
+      artist: "Novelists",
+      cover: "img23.gif",
+      file: "NOVELISTS FR - Echoes.mp3"
+    },
+    {
+      title: "Slow Dancing ",
+      artist: "Mike Dawes",
+      cover: "img12.gif",
+      file: "Slow Dancing in a Burning Room - Mike Dawes (Shows and Distancing_ Live in the USA).mp3"
+    },
+    {
+      title: "Taylor 816 - Mateus Asato jamming over Afterlife",
+      artist: "Mateus Asato ",
+      cover: "img13.webp",
+      file: "Taylor 816 - Mateus Asato jamming over Afterlife.mp3"
+    },
+
   ];
